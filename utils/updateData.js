57cef64559449c875db21153178a3221d3949d96
Timo <30553356+y21@users.noreply.github.com>
2018-05-13 02:17:13 +0200

    No wiimmfi_api parameter anymore

--- utils/updateData.js
+++ utils/updateData.js
@@ -1,4 +1,5 @@
-module.exports = async (wiimmfi_api, get) => {
+module.exports = async (get) => {
+    let wiimmfi_api = { };
     wiimmfi_api.text = JSON.parse((await get('https://wiimmfi.glitch.me/amount')).text);
     wiimmfi_api.text.regions = JSON.parse((await get('https://wiimmfi.glitch.me/regions')).text);
     wiimmfi_api.lastCheck = Date.now();
