f037bb4602251a22108971ff01436436bb3e9f27
Timo <30553356+y21@users.noreply.github.com>
2018-05-13 21:29:23 +0200

    Use the regions endpoint, mate.

--- utils/updateData.js
+++ utils/updateData.js
@@ -1,7 +1,7 @@
 module.exports = async (get) => {
     let wiimmfi_api = { };
     wiimmfi_api.text = JSON.parse((await get('https://wiimmfi.glitch.me/amount')).raw.toString());
-    wiimmfi_api.text.regions = JSON.parse((await get('https://wiimmfi.glitch.me/amount')).raw.toString());
+    wiimmfi_api.text.regions = JSON.parse((await get('https://wiimmfi.glitch.me/regions')).raw.toString());
     wiimmfi_api.lastCheck = Date.now();
     return wiimmfi_api;
 };
