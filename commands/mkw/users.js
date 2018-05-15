c6542757c56f596607fb8be61daf45332a63b9b9
Timo <30553356+y21@users.noreply.github.com>
2018-05-15 21:34:36 +0200

    Make users command better

--- commands/mkw/users.js
+++ commands/mkw/users.js
@@ -1,6 +1,17 @@
 const { get } = require("snekfetch");
+const { RichEmbed } = require("discord.js");
 
 module.exports = async message => {
-    let request = await get("http://wiimmfi.glitch.me/users");
-    message.channel.send("```js\n" + request.body.join("\n").substr(0, 1980) + "\n```")
+    const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    let request = await get("http://wiimmfi.glitch.me/users"),
+        counts = [ 0, 10, 1 ],
+        embed = new RichEmbed()
+        .setTitle("Online users - page 1")
+        .setDescription("```https\n" + request.body.slice(counts[0], counts[1]).join("\n") + "\n```")
+        .setFooter("Total players: " + request.body.length);
+        let m = await message.channel.send(embed);
+        m.react("⬅").then(m => {
+            m.message.react("➡").catch();
+        }).catch();
+        request.body = request.body.sort((a,b)=>alphabet.indexOf(a.charAt(0))-alphabet.indexOf(b.charAt(0)));
 };
