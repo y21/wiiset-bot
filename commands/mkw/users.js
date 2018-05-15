b2193b208151c2497ef59e720d6a2d9be0a165bc
Timo <30553356+y21@users.noreply.github.com>
2018-05-15 21:35:03 +0200

    Add ReactionCollector to collect reactions

--- commands/mkw/users.js
+++ commands/mkw/users.js
@@ -14,4 +14,23 @@ module.exports = async message => {
             m.message.react("➡").catch();
         }).catch();
         request.body = request.body.sort((a,b)=>alphabet.indexOf(a.charAt(0))-alphabet.indexOf(b.charAt(0)));
+        const collector = m.createReactionCollector((reaction, user) => user.id === message.author.id && (reaction.emoji.name === "➡" || reaction.emoji.name === "⬅"), { time: 60000 })
+        collector.on("collect", reaction => {
+            if(reaction.emoji.name === "➡"){
+                if(counts[2] > request.body.length) return;
+                counts[0] += 10;
+                counts[1] += 10;
+                counts[2]++;
+            } else {
+                if(counts[1] < 10) return;
+                counts[0] -= 10;
+                counts[1] -= 10;
+                counts[2]--;
+            }
+
+            embed.title = `Online users - page ${counts[2]}`;
+            embed.description = "```https\n" + request.body.slice(counts[0], counts[1]).join("\n") + "\n```";
+            m.edit(embed);
+            reaction.remove(message.author);
+        });
 };
