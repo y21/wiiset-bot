348c9fef1079cc7c6faabbb86a13684399baa8c5
Timo <30553356+y21@users.noreply.github.com>
2018-05-17 20:17:40 +0200

    This one is for you, Nightcat.

---
+++ commands/cat.js
@@ -0,0 +1,10 @@
+const { get } = require("snekfetch");
+const { Attachment } = require("discord.js");
+
+module.exports = message => {
+  get("http://aws.random.cat/meow").then(result => {
+    message.channel.send(new Attachment(result.body.file, "cat.jpg"));
+  }).catch(er => {
+      message.channel.send("I cannot access the cat api (<http://aws.random.cat/meow>), perhaps it's down?\nERR: `" + er.toString() + "`");
+  });
+};
