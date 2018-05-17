8b09f67d3381e5999eeaf0887083af965d72de78
Timo <30553356+y21@users.noreply.github.com>
2018-05-17 21:22:30 +0200

    fuuurrrr

--- commands/cat.js
+++ commands/cat.js
@@ -5,6 +5,6 @@ module.exports = message => {
   get("http://aws.random.cat/meow").then(result => {
     message.channel.send(new Attachment(result.body.file, "cat.jpg"));
   }).catch(er => {
-      message.channel.send("I cannot access the cat api (<http://aws.random.cat/meow>), perhaps it's down?\nERR: `" + er.toString() + "`");
+      message.channel.send("I cannot access the cat api (<http://aws.random.cat/meow>), perhaps it's down?\nERR: `" + er.toString().replace(/for/gi, "Fur") + "`");
   });
 };
