4159c4ed6244a62652cc8c16da9c55b26744e86f
Timo <30553356+y21@users.noreply.github.com>
2018-05-19 00:09:07 +0200

    wiiset tho

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const { commandDescriptions, prefix } = require("../config");
 
 module.exports = message => {
-    message.channel.send("This bot was made by y21 **but** is not affiliated with the official online service wiimmfi. For more information please send `" + prefix +"disclaimer`\nRepository: <https://github.com/y21/wiimmfi-bot>\nSupport Server: https://discord.gg/Mm6NWte\n\n__general__\n" + fs.readdirSync("./commands/").map(dir => {
+    message.channel.send("This bot was made by y21 **but** is not affiliated with the official online service wiimmfi. For more information please send `" + prefix +"disclaimer`\nRepository: <https://github.com/y21/wiiset-bot>\nSupport Server: https://discord.gg/Mm6NWte\n\n__general__\n" + fs.readdirSync("./commands/").map(dir => {
         if(!dir.endsWith(".js")){
             return `\n\n__${dir}__\n` + fs.readdirSync(`./commands/${dir}`).map(file => "**" + prefix + dir + " " + file.substr(0, file.indexOf(".js")) + ":** " + (commandDescriptions.find(e => e.command === file.substr(0, file.indexOf(".js"))) || { value: "command not found" }).value).join("\n")
         } else {
