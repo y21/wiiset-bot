deb01c16d3365535908d4e30bea4c293945834a3
Timo <30553356+y21@users.noreply.github.com>
2018-05-18 23:08:06 +0200

    So we can use client instead of message.client

--- commands/eval.js
+++ commands/eval.js
@@ -1,5 +1,6 @@
 module.exports = message => {
     if (message.author.id === "312715611413413889") {
+        const client = message.client;
         message.channel.send(`\`\`\`js\n${require('util').inspect(eval(message.content.split(" ").slice(1).join(" ")))}\`\`\``);
     }
 };
