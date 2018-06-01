21932b032cbc9bbb6817f2ae81f954d98a8f718f
Timo <30553356+y21@users.noreply.github.com>
2018-06-01 03:40:17 +0200

    Add early-development note

--- commands/disclaimer.js
+++ commands/disclaimer.js
@@ -1,7 +1,7 @@
 module.exports = message => {
     message.channel.send(new message.Discord.RichEmbed()
         .setTitle("Disclaimer")
-        .setDescription("This bot is **not** affiliated with wiimmfi and it's an unofficial bot. The creator of this bot is y21#0909. It uses the wiimmfi api (which is also an unofficial one since wiimmfi doesn't have an official api?).\nThe online service was made by Wiimm and Leseratte!")
+        .setDescription("This bot is **not** affiliated with wiimmfi and it's an unofficial bot. The creator of this bot is y21#0909. It uses the wiimmfi api (which is also an unofficial one since wiimmfi doesn't have an official api?).\nThe online service was made by Wiimm and Leseratte!\nAlso this bot is in early development and may have some bugs which you should immediately report by either opening an issue on the repository or by telling me on wiiset's official discord server.")
         .setColor(message.embedColors[Math.floor(Math.random() * message.embedColors.length)])
     );
 };
