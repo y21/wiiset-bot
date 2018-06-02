eeba5a71171a36680225d468ebc40825752677cb
Timo <30553356+y21@users.noreply.github.com>
2018-06-02 04:12:23 +0200

    Limit commandstats query to 10

--- commands/cmdstats.js
+++ commands/cmdstats.js
@@ -1,5 +1,5 @@
 module.exports = message => {
-    message.connection.all("SELECT * FROM commandstats ORDER BY uses DESC").then(result => {
+    message.connection.all("SELECT * FROM commandstats ORDER BY uses DESC LIMIT 10").then(result => {
         message.channel.send("```https\n" + (result.map(element => element.name + ": " + element.uses + " uses").join("\n")) + "\n```");
     });
 };
