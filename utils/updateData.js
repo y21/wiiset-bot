f539128c15b2d2cda4984f6e95816b3f6a5b187b
Timo <30553356+y21@users.noreply.github.com>
2018-06-04 21:34:46 +0200

    Store ssbb in wiimmfi_api#text

--- utils/updateData.js
+++ utils/updateData.js
@@ -2,6 +2,7 @@ module.exports = async (get) => {
     let wiimmfi_api = { };
     wiimmfi_api.text = JSON.parse((await get('https://wiimmfi.glitch.me/amount')).raw.toString());
     wiimmfi_api.text.regions = JSON.parse((await get('https://wiimmfi.glitch.me/regions')).raw.toString());
+    wiimmfi_api.text.ssbb = JSON.parse((await get("https://wiimmfi.glitch.me/ssbb/amount")).raw.toString());
     wiimmfi_api.lastCheck = Date.now();
     return wiimmfi_api;
 };
