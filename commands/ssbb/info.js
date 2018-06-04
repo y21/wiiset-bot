20c6aabf20ab98dda7107b83926e530da545c892
Timo <30553356+y21@users.noreply.github.com>
2018-06-04 21:32:40 +0200

    Manually build embed & embed color fix

--- commands/ssbb/info.js
+++ commands/ssbb/info.js
@@ -1,17 +1,29 @@
-const { get } = require("snekfetch");
-const { RichEmbed } = require("discord.js");
-
 module.exports = message => {
-    get("https://wiimmfi.glitch.me/ssbb/amount").then(result => {
-        message.channel.send(new RichEmbed()
-            .setTitle("Super Smash Bros. Brawl information")
-            .addField("Total profiles", result.body.totalProfiles)
-            .addField("Currently online", result.body.online)
-            .addField("Logins in the last 30 minutes", result.body.logins.thirty_minutes)
-            .addField("Logins in the last four hours", result.body.logins.four_hours)
-            .addField("Logins in the last 24 hours", result.body.logins.twentyfour_hours)
-            .setColor(message.embedColors[Math.floor(Math.random() * message.embedColors.length)])
-            .setTimestamp(result.body.lastEdit)
-        );
+    message.channel.send({
+        embed: {
+            title: "Super Smash Bros. Brawl information",
+            color: message.wiimmfi_api.text.ssbb.online === 0 ? 0xFF0000 : 0x00FF00,
+            fields: [{
+                name: "Total profiles",
+                value: message.wiimmfi_api.text.ssbb.totalProfiles
+            },
+                {
+                    name: "Currently online",
+                    value: message.wiimmfi_api.text.ssbb.online
+                },
+                {
+                    name: "Logins in the last 30 minutes",
+                    value: message.wiimmfi_api.text.ssbb.logins.thirty_minutes
+                },
+                {
+                    name: "Logins in the last four hours",
+                    value: message.wiimmfi_api.text.ssbb.logins.four_hours
+                },
+                {
+                    name: "Logins in the last 24 hours",
+                    value: message.wiimmfi_api.text.ssbb.logins.twentyfour_hours
+                }
+            ]
+        }
     });
 };
