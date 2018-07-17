99264d849341983ae26601ebcffa21562ee2fcb4
y21 <artur.fiedler54@gmail.com>
2018-07-17 14:20:29 +0200

    Add color to embed and use util#updateData for ctgp.recent

--- commands/ctgp/recent.js
+++ commands/ctgp/recent.js
@@ -1,24 +1,18 @@
-const {
-    get
-} = require("snekfetch");
-
-
 module.exports = async message => {
-    const result = await get("http://tt.chadsoft.co.uk/index.json");
-    if (!result.body.startsWith("{")) result.body = result.body.substr(1);
-    result.body = JSON.parse(result.body);
-    result.body.recentRecords = result.body.recentRecords.slice(0, (parseInt(message.args[1]) || 10));
+    let copy = message.wiimmfi_api.ctgp.recent;
+    copy.recentRecords = copy.recentRecords.slice(0, (parseInt(message.args[1]) || 10));
     message.channel.send({
         embed: {
             title: "CTGP Time Trials - Recent uploaded ghosts",
-            fields: result.body.recentRecords.map(val => {
+            fields: copy.recentRecords.map(val => {
                 return {
                     name: val.player || "???",
                     value: "Track: " + val.trackName +
                         "\nTrack version: " + (val.trackVersion || "Default") +
                         "\nTime: `" + val.finishTimeSimple + "`"
                 }
-            })
+            }),
+            color: (message.member || { displayColor: 0x00FF00 }).displayColor
         }
     }).catch(console.log);
 };
