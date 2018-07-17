ea57004ca0579b037e1115a8ad02ab56bb5b2688
y21 <artur.fiedler54@gmail.com>
2018-07-17 14:19:45 +0200

    Update ctgp#recent asynchronous

--- utils/updateData.js
+++ utils/updateData.js
@@ -3,6 +3,11 @@ module.exports = async (get) => {
     wiimmfi_api.text = JSON.parse((await get('https://wiimmfi.glitch.me/amount')).raw.toString());
     wiimmfi_api.text.regions = JSON.parse((await get('https://wiimmfi.glitch.me/regions')).raw.toString());
     wiimmfi_api.text.ssbb = JSON.parse((await get("https://wiimmfi.glitch.me/ssbb/amount")).raw.toString());
+    wiimmfi_api.ctgp = {
+        recent: await get("http://tt.chadsoft.co.uk/index.json")
+    };
+    if (!wiimmfi_api.ctgp.recent.body.startsWith("{")) wiimmfi_api.ctgp.recent.body = wiimmfi_api.ctgp.recent.body.substr(1);
+    wiimmfi_api.ctgp.recent = JSON.parse(wiimmfi_api.ctgp.recent.body);
     wiimmfi_api.lastCheck = Date.now();
     return wiimmfi_api;
 };
