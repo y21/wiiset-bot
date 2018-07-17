b2de388815dc305ca709798f70081755cc0c3ecd
y21 <artur.fiedler54@gmail.com>
2018-07-17 19:40:57 +0200

    Fix help page

--- commands/help.js
+++ commands/help.js
@@ -2,11 +2,11 @@ const fs = require("fs");
 const { commandDescriptions, prefix } = require("../config");
 
 module.exports = message => {
-    message.channel.send("This bot was made by y21 **but** is not affiliated with the official online service wiimmfi. For more information please send `" + prefix +"disclaimer`\nRepository: <https://github.com/y21/wiiset-bot>\nSupport Server: https://discord.gg/Mm6NWte\n\n__general__\n" + fs.readdirSync("./commands/").map(dir => {
+    message.channel.send("This bot was made by y21 **but** is not affiliated with the official online service wiimmfi. For more information please send `" + prefix +"disclaimer`\nRepository: <https://github.com/y21/wiiset-bot>\nSupport Server: https://discord.gg/6DPWSmK\n\n__general__\n" + fs.readdirSync("./commands").filter(val => val.endsWith(".js")).map(val => {
+    return `**${prefix + val.substr(0, val.indexOf(".js"))}**: ${commandDescriptions.find(val2 => val2.command === val.substr(0, val.indexOf(".js"))).value}`;
+    }).join("\n") + fs.readdirSync("./commands/").map(dir => {
         if(!dir.endsWith(".js")){
-            return `\n\n__${dir}__\n` + fs.readdirSync(`./commands/${dir}`).map(file => "**" + prefix + dir + " " + file.substr(0, file.indexOf(".js")) + ":** " + (commandDescriptions.find(e => e.command === file.substr(0, file.indexOf(".js"))) || { value: "command not found" }).value).join("\n")
-        } else {
-            return `**${prefix + dir.substr(0, dir.indexOf(".js"))}:** ${(commandDescriptions.find(e => e.command === dir.substr(0, dir.indexOf(".js"))) || { value: "command not found" }).value}`;
+            return `__${dir}__\n${fs.readdirSync(`./commands/${dir}`).map(val => "**" + prefix + dir + " " + val.substr(0, val.indexOf(".js")) + "**: " + commandDescriptions.find(val2 => val2.command === val.substr(0, val.indexOf(".js"))).value).join("\n")}`;
         }
     }).join("\n"));
 };
