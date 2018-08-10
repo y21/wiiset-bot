bc4294c5139c738722e9a548759c637ed360acc5
Timo <30553356+y21@users.noreply.github.com>
2018-08-10 20:07:04 +0200

    Improve eval command

--- commands/eval.js
+++ commands/eval.js
@@ -1,6 +1,35 @@
 module.exports = message => {
     if (message.author.id === "312715611413413889") {
-        const client = message.client;
-        message.channel.send(`\`\`\`js\n${require('util').inspect(eval(message.content.split(" ").slice(1).join(" ")))}\`\`\``);
+		try {
+			const before = Date.now();
+			const evaluation = eval(message.args.join(" "));
+			message.channel.send({ embed: {
+				title: "Evaluation",
+				color: 0x00FF00,
+				fields: [
+					{
+						name: "Type",
+						value: typeof evaluation
+					},
+					{
+						name: "Constructor",
+						value: (evaluation.constructor || { name: "/" }).name
+					},
+					{
+						name: "Evaluation time",
+						value: (Date.now() - before) + " Milliseconds"
+					}
+				],
+				description: "```js\n" + evaluation + "\n```",
+				timestamp: new Date()
+			}});
+		} catch(e) {
+			message.channel.send({ embed: {
+				title: "Evaluation",
+				color: 0xFF0000,
+				description: "```js\n" + e + "\n```",
+				timestamp: new Date()
+			}});
+		}
     }
 };
