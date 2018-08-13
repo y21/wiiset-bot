663318838e170bc35277f2154d4b1d9082b9d371
Timo <30553356+y21@users.noreply.github.com>
2018-08-13 22:27:08 +0200

    Use translations

--- commands/mod/unban.js
+++ commands/mod/unban.js
@@ -1,12 +1,12 @@
 module.exports = message => {
-	if(!message.guild) return message.reply("this command is only available in servers");
-	if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("you do not have enough permissions to ban members. (`BAN_MEMBERS`)");
+	if(!message.guild) return message.reply(message.translations.commands.guild_only || "Translation error");
+	if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply((message.translations.commands.no_permission || "Translation error").replace(/\{perm\}/g, "BAN_MEMBERS"));
 	
-	if(message.args.length === 0) return message.reply("No member to unban. You either did not provide any GuildMember (User ID, User Tag (User#1234) or mention");
+	if(message.args.length === 0) return message.reply(message.translations.commands.no_member_unban);
 	
 	message.guild.unban(message.args[1]).then(u => {
-		message.reply(`__${u.tag}__ has been unbanned.`).catch(console.log);
+		message.reply((message.translations.commands.unbanned || "Translation error").replace(/\{target\}/g, u.tag)).catch(console.log);
 	}).catch(e => {
-		message.reply(`an error occured while trying to unban a GuildMember. \`${e.toString()}\``).catch(console.log);
+		message.reply(message.translations.commands.unban_error + "Error: `" + e.toString() + "`").catch(console.log);
 	});
 };
