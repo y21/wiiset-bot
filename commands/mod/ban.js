16ea404f441579a4e4b0c4dcf5c293dc4d7c4110
Timo <30553356+y21@users.noreply.github.com>
2018-08-13 03:25:18 +0200

    Use translations and fix MessageOptions

--- commands/mod/ban.js
+++ commands/mod/ban.js
@@ -1,18 +1,16 @@
 module.exports = async message => {
 	try {
-		if(!message.guild) return message.reply("this command is only available in servers");
-		if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("you do not have enough permissions to ban members. (`BAN_MEMBERS`)");
+		if(!message.guild) return message.reply(message.translations.commands.guild_only || "Translation error");
+		if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply((message.translations.commands.no_permissions || "Translation error").replace(/\{perm\}/g, "BAN_MEMBERS"));
 		
 		let target;
 		
 		if(message.mentions.members.size === 0){
 			target = message.guild.member(message.client.users.find(val => val.id === message.args[1] || val.tag === message.args[1]));
 		} else target = message.mentions.members.first();
-		if(!target) return message.reply("No member to ban. You either did not provide any GuildMember (User ID, User Tag (User#1234) or mention");
+		if(!target) return message.reply(message.translations.commands.no_member_softban || "Translation error");
 		
-	message.reply(`Do you really want to ban __${target.user.tag}__? Reply with either __y__es or __n__o within the next 25 seconds.\n\nReason: ${message.args.slice(2).join(" ") || "- none -"}`, {
-			disableEveryone: true
-		});
+		message.reply((message.translations.commands.ban_confirmation || "Translation error").replace(/\{target\}/g, target.user.tag));
 		
 		const collector = message.channel.createMessageCollector(m => {
 			return m.author.id === message.author.id && (m.content === "y" || m.content === "yes" || m.content === "n" || m.content === "no");
@@ -24,9 +22,9 @@ module.exports = async message => {
 		collector.on("collect", m => {
 			if(m.content === "y" || m.content === "yes"){
 				target.ban({ days: 7, reason: "Ban requested by " + message.author.tag + " | Reason: " + (message.args.slice(2).join(" ") || "- none -")}).then(() => {
-					message.channel.send(`__${target.user.tag}__ has been banned.`);
+					message.channel.send((message.translations.commands.banned || "Translation error").replace(/\{target\}/g, target.user.tag));
 				}).catch(err => {
-					message.reply("could not ban the targeted user. `" + err.toString() + "`");
+					message.reply((message.translations.commands.guild_only || "Translation error") + " `" + err.toString() + "`");
 				});
 			} else {
 				message.reply("Ban aborted.");
