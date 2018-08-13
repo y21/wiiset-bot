cd8fef655f82ea4a6baeb52f978a583341e01ea3
Timo <30553356+y21@users.noreply.github.com>
2018-08-13 03:12:57 +0200

    Use translations and rem. disableEveryone MessageOption
    ... because it is enabled as a ClientOption

--- commands/playing.js
+++ commands/playing.js
@@ -1,5 +1,3 @@
 module.exports = message => {
-	message.reply("`" + message.client.users.filter(u => (u.presence.game || { name: 0 }).name === message.args.join(" ")).size + "` people are playing `" + message.args.join(" ").replace(/`/g, "") + "`", {
-		disableEveryone: true
-	});
+	message.reply((message.translations.commands.playing || "Translation error").replace(/\{amount\}/g, message.client.users.filter(u => (u.presence.game || { name: 0 }).name === message.args.join(" ")).size).replace(/\{game\}/g, message.args.join(" ").replace(/`/g, "")));
 }
