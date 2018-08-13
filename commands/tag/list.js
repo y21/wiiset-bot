cf59f7df365d1728b67823cc8cde55ba2b007bdd
Timo <30553356+y21@users.noreply.github.com>
2018-08-14 00:56:08 +0200

    Use translations (tag/list)

--- commands/tag/list.js
+++ commands/tag/list.js
@@ -6,11 +6,11 @@ module.exports = message => {
 	connection.all(`SELECT * FROM tags ORDER BY uses DESC LIMIT ${limit}`).then(res => {
 		message.channel.send({ embed: {
 			title: "Tag list",
-			description: `A list of ${limit} tags has been loaded in ${Date.now() - timestamp} milliseconds. To view a tag, type \`${message.prefix}tag <tagname>\``,
+			description: (message.translations.commands.tag_list_heading || "Translation error").replace(/\{amount\}/g, limit).replace(/\{time\}/g, Date.now() - timestamp).replace(/\{prefix\}/g, message.prefix),
 			fields: res.map(element => {
 				return {
 					name: element.name,
-					value: `Uses: ${element.uses} | Created at ${new Date(parseInt(element.createdAt)).toLocaleString()}`
+					value: (message.translations.commands.tag_list_value || "Translation error").replace(/\{amount\}/g, element.uses).replace(/\{date\}/g, new Date(parseInt(element.createdAt)).toLocaleString())
 				};
 			})
 		}});
