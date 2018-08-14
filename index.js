7fcf5c6e5d3394f3ea1a2c859c4c0a731a3b20dc
Timo <30553356+y21@users.noreply.github.com>
2018-08-14 22:42:11 +0200

    Fix Wiiset being spammy

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ sqlite.open("./database.sqlite");
 const messages = new Map();
 
 client.on("message", async message => {
-    if(!message.guild) return message.channel.send("⛔ I don't work in direct messages!");
+    if(!message.guild && message.author.id !== client.user.id) return message.channel.send("⛔ I don't work in direct messages!");
     if(production && message.author.id !== "312715611413413889") return;
     if (message.author.bot || !message.content.startsWith(prefix)) return;
     message.command = message.content.substr(prefix.length, (message.content.indexOf(" ") > -1 ? message.content.indexOf(" ") - prefix.length : message.content.length))
