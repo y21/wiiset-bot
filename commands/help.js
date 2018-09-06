3ca62d6e21f359fa0911651b4961ed6fab5dbc0a
Timo <30553356+y21@users.noreply.github.com>
2018-09-06 17:41:54 +0200

    Help page rewrite

--- commands/help.js
+++ commands/help.js
@@ -2,42 +2,74 @@ const { readdirSync } = require("fs"),
 	{ commandDescriptions, prefix } = require("../config.json");
 
 module.exports = async message => {
-	const sub = readdirSync("commands"),
-		categories = sub.filter(val => !val.endsWith(".js")),
-		commands = sub.filter(val => val.endsWith(".js")).map(val2 => val2.substr(0, val2.indexOf(".js"))).concat(categories.map(val2 => readdirSync("commands/" + val2).map(val3 => `${val2}/${val3.substr(0,val3.indexOf(".js"))}`)));
-		// Add so-called "general" category
-		categories.unshift("general");
-		let embed = {
-			title: "Help | General | Page 0",
-			color: 0xFFFFFF,
-			fields: commands.filter(val => !val.includes("/") && val.constructor.name !== "Array").map(val => { return {
-				name: `${prefix + val}`,
-				value: commandDescriptions[val] || "???"
-			}})
-		};
-		let msg = await message.channel.send({embed: embed}),
-		counter = 0;
-		msg.react("⬅").then(m => {
-            msg.react("➡").catch();
-        }).catch();
-		const collector = msg.createReactionCollector((reaction, user) => user.id === message.author.id && (reaction.emoji.name === "➡" || reaction.emoji.name === "⬅"), { time: 60000 });
-		collector.on("collect", reaction => {
-			// ignore if <page 1 or >categories.length
-			if(reaction.emoji.name === "⬅" && counter === 1) return;
-			if(reaction.emoji.name === "➡" && counter === categories.length - 2) return;
-			
-			let left = reaction.emoji.name === "⬅";
-			if(left) --counter;
-			else ++counter;
-			if(counter === 1) msg.edit({ embed: embed }).catch();
-			msg.edit({ embed: {
-				title: `Help | ${categories[counter + 1]} | Page ${counter}`,
-				color: 0xFFFFFF,
-				fields: (commands.filter(val => val.constructor.name === "Array")[counter] || []).map(val => { return {
-					name: prefix + val.replace(/\//g, " "),
-					value: commandDescriptions[val]
-				}})
-			}});
-			reaction.remove(message.author).catch();
-		});
+	const commandCategories = {
+		1: undefined,
+		2: "general",
+		3: "ctgp",
+		4: "mkw",
+		5: "mod",
+		6: "ssbb",
+		7: "tag"
+	};
+	let page = 1;
+	const commands = {
+		no_category: readdirSync("./commands/").filter(val => val.endsWith(".js"))
+	};
+	for(const directory of readdirSync("./commands").filter(val => !val.endsWith(".js"))) {
+		commands[directory] = readdirSync("./commands/" + directory);
+	}
+	// Page 1
+	const msg = await message.channel.send({embed: {
+		title: "Help page | Page " + page + " | " + commandCategories[page],
+		description: "This bot is being developed by y21. It is using several APIs such as the *(unofficial)* [Wiimmfi API](https://github.com/y21/wiimmfi-api) to get data about Wiimmfi (rooms, user information etc.) and the [Time Trial API](http://tt.chadsoft.co.uk/index.json) made by chadderz to get information about ghosts, tracks and more.\n" +
+					"The code might have some bugs and the author of this bot is planning to rewrite it soon, therefore you should immediately report them.\n" +
+					"**Note:** This bot is *not* affiliated with Wiimmfi.\n" + 
+					"This help page will only work for the next 180 seconds (3 minutes).",
+		fields: [
+			{
+				name: "Related links",
+				value: "- Any questions or problems? Join our [support server](https://discord.gg/6DPWSmK).\n" +
+						"- [Invite me](https://discordapp.com/api/oauth2/authorize?client_id=440210686954569739&permissions=8&scope=bot) (administrator permissions, needed for some commands)\n" +
+						"- [Invite me](https://discordapp.com/api/oauth2/authorize?client_id=440210686954569739&permissions=0&scope=bot) (no permissions)\n" +
+						"- Want to see how this bot works? Take a look into the [source](https://github.com/y21/wiiset-bot).\n" +
+						"- Read more about this bot [here](https://y21.github.io/wiiset-bot/)."
+			}
+		],
+		color: message.member.displayColor || 0x000000
+	}}).catch(console.log);
+
+	await msg.react("⬅");
+	await msg.react("➡");
+	
+	const collector = msg.createReactionCollector((reaction, user) => reaction.emoji.name === "➡" || reaction.emoji.name === "⬅", {
+		time: 180000
+	});
+
+	collector.on("collect", reaction => {
+		if (reaction.count === 1) return; // ignore itself
+		if (reaction.emoji.name === "➡") {
+			if (page - 1 >= Object.keys(commands).length) return;
+			++page;
+		} else {
+			if (page <= 2) return;
+			--page;
+		}
+		msg.edit({embed: {
+			title: `Help page | Page ${page} | ${commandCategories[page]}`,
+			fields: [undefined, ...Object.values(commands)][page - 1].map(file => {
+				return {
+					name: prefix + (page === 2 ? file.substr(0, file.indexOf(".js")) : commandCategories[page] + "/" + file.substr(0, file.indexOf(".js"))).replace(/\//g, " "),
+					value: commandDescriptions[page === 2 ? file.substr(0, file.indexOf(".js")) : commandCategories[page] + "/" + file.substr(0, file.indexOf(".js"))] || "Command description not found."
+				}
+			}),
+			color: message.member.displayColor || 0x000000
+		}}).then(async () => {
+			await reaction.remove(message.author).catch();
+		}).catch();
+	});
+
+	collector.on("end", () => {
+		msg.reactions.filter(reaction => reaction.me).map(async reaction => await reaction.remove().catch());
+		collector.stop();
+	});
 }
