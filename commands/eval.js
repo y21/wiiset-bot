81d83e2a83cb7e2e7d4f6af201553e2579481062
Timo <30553356+y21@users.noreply.github.com>
2018-09-16 02:50:07 +0200

    Eval command updated

--- commands/eval.js
+++ commands/eval.js
@@ -1,35 +1,49 @@
-module.exports = message => {
-    if (message.author.id === "312715611413413889") {
-		try {
-			const before = Date.now();
-			const evaluation = eval(message.args.join(" ").replace(/```.*/g, ""));
-			message.channel.send({ embed: {
-				title: "Evaluation",
-				color: 0x00FF00,
-				fields: [
-					{
-						name: "Type",
-						value: typeof evaluation
-					},
-					{
-						name: "Constructor",
-						value: (evaluation.constructor || { name: "/" }).name
-					},
-					{
-						name: "Evaluation time",
-						value: (Date.now() - before) + " Milliseconds"
-					}
-				],
-				description: "```js\n" + evaluation + "\n```",
-				timestamp: new Date()
-			}});
-		} catch(e) {
-			message.channel.send({ embed: {
-				title: "Evaluation",
-				color: 0xFF0000,
-				description: "```js\n" + e + "\n```",
-				timestamp: new Date()
-			}});
-		}
+module.exports = async message => {
+    if (message.author.id !== "312715611413413889") return;
+    if (message.args.length < 1) return message.reply("Nothing to evaluate...");
+
+    const embed = {
+        color: (message.member || {
+            displayColor: 0x00FF00
+        }).displayColor,
+        title: "Evaluation",
+        fields: [{
+            name: "Input",
+            value: "```js\n" + message.args.join(" ") + "\n```"
+        }]
+    };
+    const oldts = Date.now();
+    let evaluation;
+    try {
+        evaluation = eval(message.args.join(" "));
+        if (typeof evaluation !== "undefined") {
+            if (evaluation.constructor.name === "Promise") {
+                embed.fields.push({
+                    name: "Output",
+                    value: "```js\n" + require("util").inspect(await evaluation).substr(0, 1000) + "\n```"
+                });
+            } else embed.fields.push({
+                name: "Output",
+                value: "```js\n" + require("util").inspect(evaluation).substr(0, 1000).replace(message.client.token, "[TOKEN REDACTED]") + "\n```"
+            });
+        } else embed.fields.push({
+            name: "Output",
+            value: "```js\n" + evaluation.substr(0, 1000) + "\n```"
+        });
+    } catch (e) {
+        embed.fields.push({
+            name: "Output",
+            value: "```js\n" + e + "\n```"
+        });
+        embed.color = 0xFF0000
     }
+
+    embed.fields.push({
+        name: "Evaluation time",
+        value: (Date.now() - oldts) + " Milliseconds"
+    });
+
+    message.channel.send({
+        embed
+    });
 };
