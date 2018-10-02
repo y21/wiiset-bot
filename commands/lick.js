13225c0984a20be2d0fee519c3713d66258062df
Timo <30553356+y21@users.noreply.github.com>
2018-10-02 19:13:09 +0200

    Use UserResolver to accept tags

--- commands/lick.js
+++ commands/lick.js
@@ -1,19 +1,25 @@
 const { RichEmbed } = require("discord.js");
+const { getByTag } = require("../utils/UserResolver");
+
 module.exports = message => {
-    if(message.mentions.users.size == 0) return message.reply("who to lick?!");
-    message.connection.get("SELECT * FROM licks WHERE id='" + message.mentions.users.first().id + "'").then(r => {
+	let target;
+    if (message.args.length === 0) return message.reply("who to lick?!");
+	if (message.mentions.users.size > 0) target = message.mentions.users.first();
+	else if (/^.+#\d{4}$/.test(message.args[0])) target = getByTag(message.client.users, message.args[0]);
+	if (target === undefined) return message.reply("who to lick?!");
+    message.connection.get("SELECT * FROM licks WHERE id='" + target.id + "'").then(r => {
         let licks = 1;
-        if(!r) message.connection.run("INSERT INTO licks VALUES ('" + message.mentions.users.first().id + "', 1)");
+        if(!r) message.connection.run("INSERT INTO licks VALUES ('" + target.id + "', 1)");
         else {
-            message.connection.run(`UPDATE licks SET amount=${r.amount + 1} WHERE id='${message.mentions.users.first().id}'`);
+            message.connection.run(`UPDATE licks SET amount=${r.amount + 1} WHERE id='${target.id}'`);
             licks = r.amount + 1;
         }
         message.channel.send(
             new RichEmbed()
-                .setAuthor(message.mentions.users.first().tag, message.mentions.users.first().avatarURL, message.mentions.users.first().avatarURL)
+                .setAuthor(target.tag, target.avatarURL, target.avatarURL)
                 .setColor(0x77b254)
                 .addField("Lick User", message.translations.commands.lick_user || "Translation error")
-                .addField("Total licks", (message.translations.commands.total_licks || "Translation error").replace(/\{amount\}/g, licks).replace(/\{user\}/g, message.mentions.users.first().username))
+                .addField("Total licks", (message.translations.commands.total_licks || "Translation error").replace(/\{amount\}/g, licks).replace(/\{user\}/g, target.username))
                 .setFooter(`Licked by ${message.author.tag}`, message.author.avatarURL)
         );
     }).catch(err => {
