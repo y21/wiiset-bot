bb9bc726b01e5e5431f8def44d8e95ac173674da
Timo <30553356+y21@users.noreply.github.com>
2018-10-03 15:42:12 +0200

    Use "IF NOT EXISTS" SQL statement instead of hardcode

--- commands/lick.js
+++ commands/lick.js
@@ -1,28 +1,41 @@
-const { RichEmbed } = require("discord.js");
 const { getByTag } = require("../utils/UserResolver");
 
 module.exports = message => {
-	let target;
+    let target;
     if (message.args.length === 0) return message.reply("who to lick?!");
-	if (message.mentions.users.size > 0) target = message.mentions.users.first();
-	else if (/^.+#\d{4}$/.test(message.args[0])) target = getByTag(message.client.users, message.args[0]);
-	if (target === undefined) return message.reply("who to lick?!");
+    if (message.mentions.users.size > 0) target = message.mentions.users.first();
+    else if (/^.+#\d{4}$/.test(message.args[0])) target = getByTag(message.client.users, message.args[0]);
+    if (target === undefined) return message.reply("who to lick?!");
     message.connection.get("SELECT * FROM licks WHERE id='" + target.id + "'").then(r => {
         let licks = 1;
-        if(!r) message.connection.run("INSERT INTO licks VALUES ('" + target.id + "', 1)");
+        if (!r) message.connection.run("INSERT INTO licks VALUES ('" + target.id + "', 1)");
         else {
             message.connection.run(`UPDATE licks SET amount=${r.amount + 1} WHERE id='${target.id}'`);
             licks = r.amount + 1;
         }
-        message.channel.send(
-            new RichEmbed()
-                .setAuthor(target.tag, target.avatarURL, target.avatarURL)
-                .setColor(0x77b254)
-                .addField("Lick User", message.translations.commands.lick_user || "Translation error")
-                .addField("Total licks", (message.translations.commands.total_licks || "Translation error").replace(/\{amount\}/g, licks).replace(/\{user\}/g, target.username))
-                .setFooter(`Licked by ${message.author.tag}`, message.author.avatarURL)
-        );
+        message.channel.send({
+            embed: {
+                color: 0x77b254,
+                author: {
+                    name: target.tag,
+                    icon_url: target.displayAvatarURL
+                },
+                fields: [{
+                        name: "Lick User",
+                        value: message.translations.commands.lick_user || "Translation error",
+                    },
+                    {
+                        name: "Total licks",
+                        value: (message.translations.commands.total_licks || "Translation error").replace(/\{amount\}/g, licks).replace(/\{user\}/g, target.username)
+                    },
+                ],
+                footer: {
+                    text: `Licked by ${message.author.tag}`,
+                    icon_url: message.author.displayAvatarURL
+                }
+            }
+        });
     }).catch(err => {
-        if(err.toString().includes("no such table: licks")) message.connection.run("CREATE TABLE `licks` (`id` TEXT, `amount` INTEGER);");
+        message.connection.run("CREATE TABLE IF NOT EXISTS `licks` (`id` TEXT, `amount` INTEGER);");
     });
 };
