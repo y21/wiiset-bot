3ff9a0bf9043beaf63e67e626b0e6528266c7705
Timo <artur.fiedler54@gmail.com>
2019-08-18 16:17:59 +0200

    update database structure

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const Base = require("./Base");
+const Server = require("./server/Server");
 const base = new Base({
     disableEveryone: true
 });
+const server = new Server(3000 || process.env.PORT, [
+    {
+        method: "get",
+        path: "/",
+        run: (req, res) => res.send(Server.serve("./server/client/index.html"))
+    },
+    {
+        method: "get",
+        path: "/api/usage",
+        run: (req, res) => {
+            base.sqlite.all("SELECT * FROM usageLogs").then(r => {
+                res.json(r);
+            });
+        }
+    }
+]);
+server.init();
 
 base.initializeCommands().then(() => {
     base.initializeUtils().then(() => {
         base.initializeLanguages().then(() => {
             base.openDatabase().then(async () => {
-                await base.sqlite.run("CREATE TABLE IF NOT EXISTS commandstats (`name` TEXT, `uses` INTEGER)");
+                await base.sqlite.run("CREATE TABLE IF NOT EXISTS commandstats (`name` TEXT, `uses` INTEGER, `lastUsage` TEXT)");
                 await base.sqlite.run("CREATE TABLE IF NOT EXISTS licks (`id` TEXT, `amount` INTEGER)");
                 await base.sqlite.run("CREATE TABLE IF NOT EXISTS tags (`name` TEXT, `author` TEXT, `content` TEXT, `createdAt` TEXT, `uses` INTEGER)");
                 await base.sqlite.run("CREATE TABLE IF NOT EXISTS languages (`guild` TEXT, `lang` TEXT)");
                 await base.sqlite.run("CREATE TABLE IF NOT EXISTS pids (`user` TEXT, `pid` TEXT)");
+                await base.sqlite.run("CREATE TABLE IF NOT EXISTS usageLogs (`month` INTEGER, `uses` INTEGER)");
             });
         });
     });
