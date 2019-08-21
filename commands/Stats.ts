74d0da6f9b329f8e92b7d22599de7fb217d1199d
Timo <artur.fiedler54@gmail.com>
2019-08-22 00:04:48 +0200

    stats command

---
+++ commands/Stats.ts
@@ -0,0 +1,15 @@
+import Command from "../structures/Command";
+import Base from "../Base";
+
+export default <Command>{
+    name: "stats",
+    description: "Sends general information about the bot, like server count and ping",
+    args: [],
+    guildOnly: false,
+    category: null,
+    ownerOnly: false,
+    run: (base: Base, message: any) => {
+
+        return ["it works!!!"];
+    }
+}
