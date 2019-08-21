58b6a2f76ab49a6c56ae851df212e02d542cd926
Timo <artur.fiedler54@gmail.com>
2019-08-22 00:45:51 +0200

    add cooldown check and store in database

--- events/Message.ts
+++ events/Message.ts
@@ -25,6 +25,23 @@ export default <Event>{
         if (!command) return;
         if (command.category !== null && command.category !== arg1) return;
         if (command.ownerOnly && message.author.id !== Base.owner.id) return message.reply("You cannot execute this command.");
+
+        // Cooldown check
+        {
+            const lastMsg = base.messages.get(message.author.id);
+            if (lastMsg !== undefined && Date.now() - lastMsg < 1000)
+                return message.reply("no spamming.");
+            else base.messages.set(message.author.id, Date.now());
+        }
+
+        // Increase command in database
+        base.sqlite.get("SELECT * FROM commandstats WHERE name=?", (command.category ? command.category + "." + command.name : command.name)).then((result: any) => {
+            if (!result && command) {
+                base.sqlite.run("INSERT INTO commandstats VALUES (?, 1, ?)", (command.category ? command.category + "." + command.name : command.name), Date.now());
+            } else if (command)
+                base.sqlite.run("UPDATE commandstats SET uses = uses + 1, lastUsage=? WHERE name=?", Date.now(), (command.category ? command.category + "." + command.name : command.name));
+        });
+
         let commandResponse: string[] | undefined;
         try {
             commandResponse = await command.run(base, message);
