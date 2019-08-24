acfc7b4d46b103505a2734b5a9d58bdad74ac9af
Timo <artur.fiedler54@gmail.com>
2019-08-24 17:15:45 +0200

    other stuff

--- utils/UserResolver.js
+++ utils/UserResolver.js
@@ -1,11 +1,13 @@
-module.exports = class UserResolver {
-	static extract(client, string, includes) {
-		return client.users.get(string) || 
-			client.users.find(v => v.username === string) || 
-			(includes ? client.users.find(v => v.username.includes(string)) : undefined);
-	}
-	
-	static getByTag(UserCollection, tag) {
-		return UserCollection.find(v => v.tag === tag);
-	}
+"use strict";
+exports.__esModule = true;
+exports["default"] = {
+    extract: function (client, source, includes) {
+        if (includes === void 0) { includes = false; }
+        return client.users.get(source) ||
+            client.users.find(function (v) { return v.username === source; }) ||
+            (includes ? client.users.find(function (v) { return v.username.includes(source); }) : undefined);
+    },
+    getByTag: function (UserCollection, tag) {
+        return UserCollection.find(function (v) { return v.tag === tag; });
+    }
 };
