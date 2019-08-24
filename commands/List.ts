d0a0e7e6dd12b68013c4edd1fd88dec3b77e66ed
Timo <artur.fiedler54@gmail.com>
2019-08-24 17:08:34 +0200

    update list command

--- commands/List.ts
+++ commands/List.ts
@@ -11,7 +11,40 @@ export default <Command>{
     ownerOnly: false,
     run: (base: Base, message: any) => {
         return new Promise(async (a: any, b: any) => {
-
+            fetch(`${Base.wiimmfiAPI}/mkw/amount`).then((res: any) => res.json())
+                .then((res: any) => {
+                    a([{
+                        embed: {
+                            title: "Mario Kart Wii Room Overview",
+                            thumbnail: {
+                                url: "http://chadsoft.co.uk/wiimmfi/wiimmfi-dark.png"
+                            },
+                            color: 0x00FF00,
+                            fields: [
+                                {
+                                    name: "Worldwide rooms",
+                                    value: res.available.worldwides || "?"
+                                },
+                                {
+                                    name: "Continental rooms",
+                                    value: res.available.continentals || "?"
+                                },
+                                {
+                                    name: "Private rooms",
+                                    value: res.available.privates || "?"
+                                },
+                                {
+                                    name: "Total players",
+                                    value: res.available.players || "?"
+                                }
+                            ],
+                            footer: {
+                                text: "Data will be fetched and refreshed every 5 minutes.",
+                                icon_url: message.author.displayAvatarURL
+                            }
+                        }
+                    }]);
+                }).catch(b);
         });
     }
 }
