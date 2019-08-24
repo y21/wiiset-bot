c604f4868e461cd0f5425bafe29ec51ede39c056
Timo <artur.fiedler54@gmail.com>
2019-08-24 17:11:50 +0200

    add structures folder

---
+++ structures/Ghost.ts
@@ -0,0 +1,14 @@
+export interface Ghost {
+    trackName: string | undefined;
+    trackVersion: string | undefined;
+    finishTimeSimple: string;
+    bestSplitSimple: string;
+    stars: {
+        gold: number;
+        silver: number;
+        bronze: number;
+    };
+    "200cc": boolean;
+    player: string | undefined;
+    country: string | undefined;
+}
