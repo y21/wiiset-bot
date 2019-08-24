e942919f997d28efa057e6d8ab9d1f6375244134
Timo <artur.fiedler54@gmail.com>
2019-08-24 17:08:59 +0200

    add softban command

---
+++ commands/Softban.ts
@@ -0,0 +1,53 @@
+import Command from "../structures/Command";
+import Base from "../Base";
+import {GuildMember, Message, MessageCollector} from "discord.js";
+
+export default <Command>{
+    name: "softban",
+    description: "Softbans a user from this server: bans a user and unbans them immediately after to delete all messages sent by this user in the past.",
+    args: [{
+        name: "user",
+        description: "The user to softban. This needs to be either a mention or a user ID.",
+        required: true
+    }],
+    guildOnly: true,
+    category: "mod",
+    ownerOnly: false,
+    run: async (base: Base, message: Message) => {
+        if (!message.member.hasPermission("BAN_MEMBERS"))
+            throw new Error("You don't have permissions to ban users which is required for the softban command.");
+        let target: GuildMember | undefined;
+        const args: string[] = message.content.split(" ").slice(1);
+        if (message.mentions.members.size > 0)
+            target = message.mentions.members.first();
+        else if (/^\d{17,20}$/.test(args[1]))
+            target = message.guild.members.get(args[1]) || await message.guild.fetchMember(args[1], false);
+
+        if (!target) throw new Error("User not found");
+
+        const confirmMessage: Message | Message[] = await message.channel.send(`Do you really want to softban __${target.user.tag}__? Reply with either **y** or **n** in the next 15 seconds to confirm your softban.\n\nReason: \`${args.slice(2).join(" ")}\``);
+        if (Array.isArray(confirmMessage))
+            throw new Error("An internal error occurred: TextChannel.send(...) returned an array of messages.");
+        const collector: MessageCollector = message.channel.createMessageCollector((m: Message) => true, {
+            time: 15000,
+            maxMatches: 1
+        });
+
+        collector.on("collect", (msg: Message) => {
+            if (!target) return;
+            if (msg.content.toLowerCase() === "y" || msg.content.toLowerCase() === "yes") {
+                target.ban({
+                    days: 7,
+                    reason: "Banned by " + message.author.tag + ". Reason: " + args.slice(2).join(" ")
+                }).then(() => {
+                    if (!target) return;
+                    message.guild.unban(target.id).then(() => {
+                        confirmMessage.edit("Softbanned.");
+                    });
+                });
+            } else if (msg.content.toLowerCase() === "n" || msg.content.toLowerCase() === "no") {
+                confirmMessage.edit("Softban aborted.");
+            }
+        });
+    }
+}
