b5330568c266dc3bc8e6845f99380aa26b105e57
Timo <artur.fiedler54@gmail.com>
2020-01-05 02:43:12 +0100

    add viewtag command

---
+++ src/commands/ViewTag.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: "tag",
+    ownerOnly: false,
+    guildOnly: false,
+    run: (context, args) => {
+        return new Promise((resolve, reject) => {
+            context.pool.query("SELECT content FROM tags WHERE name = $1",
+                [args[0]],
+                (_, res) => {
+                    if (res.rowCount === 0) {
+                        reject(["⚠️ `Tag not found...`"]);
+                    } else {
+                        resolve([{
+                            embed: {
+                                description: res.rows[0].content
+                            }
+                        }]);
+                    }
+                });
+        });
+    }
+};
