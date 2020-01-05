621e0d77945ef0630502bf07441fa82eed42b034
Timo <artur.fiedler54@gmail.com>
2020-01-05 02:33:40 +0100

    improved tag list

--- src/commands/TagList.js
+++ src/commands/TagList.js
@@ -4,12 +4,12 @@ module.exports = {
     guildOnly: false,
     run: (context) => {
         return new Promise((resolve) => {
-            context.pool.query("SELECT name FROM tags LIMIT 10",
+            context.pool.query("SELECT name, uses FROM tags ORDER BY uses DESC LIMIT 10",
                 (_, {rows}) => {
                     resolve([{
                         embed: {
                             title: "Tag List",
-                            description: rows.map((k, i) => `${i}. ${k.name}`).join("\n")
+                            description: rows.map((k, i) => `${i + 1}. ${k.name} (used ${k.uses} times)`).join("\n")
                         }
                     }]);
                 });
