f81186f85663abd4f347db727daa816776cb7cda
Timo <artur.fiedler54@gmail.com>
2020-01-05 02:27:04 +0100

    wrong quotes

--- src/commands/CreateTag.js
+++ src/commands/CreateTag.js
@@ -7,7 +7,7 @@ module.exports = {
     run: (context, args) => {
         return new Promise((resolve, reject) => {
             if (!TagRegex.test(args[0])) throw new Error("Invalid tag name");
-            context.pool.query(`INSERT INTO tags ("name", "author", "content", "createdAt", "uses") VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING`,
+            context.pool.query("INSERT INTO tags (\"name\", \"author\", \"content\", \"createdAt\", \"uses\") VALUES ($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING",
                 [args[0], context.userId, args.slice(1).join(" "), Date.now().toString(), 0],
                 (_, res) => {
                     if (res.rowCount === 0) {
