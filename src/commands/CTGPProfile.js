463da785dab5c2aae3270777e1ac0e62d62cdce9
Timo <artur.fiedler54@gmail.com>
2020-01-06 19:12:46 +0100

    ctgpprofile prototype

---
+++ src/commands/CTGPProfile.js
@@ -0,0 +1,12 @@
+const fetch = require("node-fetch");
+const { ctgpAPI } = require("../../configs/apis");
+
+
+module.exports = {
+    name: "ctgpprofile",
+    ownerOnly: false,
+    guildOnly: false,
+    run: async () => {
+        
+    }
+};
