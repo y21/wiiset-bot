120c92bf0ae387f7694c7ca2aeed93cab06bd813
Timo <artur.fiedler54@gmail.com>
2020-01-06 11:19:05 +0100

    add deletetag command

---
+++ src/commands/DeleteTag.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "deletetag",
+    ownerOnly: false,
+    guildOnly: false,
+    run: (context, args) => {
+        return new Promise((resolve, reject) => {
+            context.pool.query("DELETE FROM tags WHERE author = $1 AND name = $2",
+                [context.userId, args[0]],
+                (_, res) => {
+                    if (res.rowCount === 0) {
+                        reject(["⚠️ `Tag not found...`"]);
+                    } else {
+                        resolve(["✅ Tag deleted"]);
+                    }
+                });
+        });
+    }
+};
