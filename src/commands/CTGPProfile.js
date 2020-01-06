478e5b094c9080a7879698a131aa67edd4e550a3
Timo <artur.fiedler54@gmail.com>
2020-01-06 19:27:21 +0100

    argument validation

--- src/commands/CTGPProfile.js
+++ src/commands/CTGPProfile.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const { ctgpAPI } = require("../../configs/apis");
+const { CTGPProfileID, Snowflake } = require("../structures/Constants").Regexes;
 
 
 module.exports = {
@@ -10,6 +11,16 @@ module.exports = {
         return new Promise((resolve, reject) => {
             if (args.length === 0)
                 return reject(["⚠️ `No arguments provided...`"]);
+            let pid;
+            if (Snowflake.test(args[0])) {
+                // todo: get pid from database
+            } else if (!CTGPProfileID.test(args[0])) {
+                return reject(["⚠️ `Invalid Profile ID provided...`"]);
+            } else {
+                pid = args[0];
+            }
+
+
         });
     }
 };
