c5f8ea89a175c20362af9521af0b33b98194f303
Timo <artur.fiedler54@gmail.com>
2020-01-08 22:58:28 +0100

    improve TrackHelper structure

--- src/structures/TrackHelper.js
+++ src/structures/TrackHelper.js
@@ -1,12 +1,27 @@
 module.exports = class TrackHelper {
-    constructor(tracks) {
+    constructor(rest, tracks) {
+        this.rest = rest;
         if (typeof tracks === "undefined")
-            this.tracks = TrackHelper.getTracks();
+            this.init();
         else
             this.tracks = tracks;
     }
 
-    static getTracks() {
+    async init() {
+        this.tracks = await TrackHelper.getTracks();
+    }
+
+    static async getTracks() {
+        const tracks = {
+            original: (await this.rest.getOriginalTracks()).map(v => ({ ...v, category: "original" })),
+            custom: (await this.rest.getCustomTracks()).map(v => ({ ...v, category: "custom" }))
+        };
         
+        return tracks.original.concat(tracks.custom).map(v => ({
+            name: v.name,
+            id: v.trackId,
+            href: v._links.item.href,
+            category: v.category
+        }));
     }
 }
