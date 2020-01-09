faa5ac4f0d2597fe52da8b66e57167ef74bb3d7f
Timo <artur.fiedler54@gmail.com>
2020-01-09 18:34:05 +0100

    add getMKWRoomStats function

--- src/rest/WiimmfiRest.js
+++ src/rest/WiimmfiRest.js
@@ -1,6 +1,6 @@
 const fetch = require("node-fetch");
 
-module.exports = class WiimmfiRest {
+class WiimmfiRest {
     constructor(host) {
         this.host = host;
     }
@@ -23,4 +23,29 @@ module.exports = class WiimmfiRest {
             .then(v => v.json())
             .then(v => v.slice(0, limit));
     }
+
+    getMKWRoomStats(id) {
+        return fetch(`${WiimmfiRest.originHost}/mkw?m=json`)
+            .then(v => v.json())
+            .then(v => {
+                const targetRoom = v.find(r => r.room_id === id || r.room_name === id);
+                if (!targetRoom)
+                    throw new Error("Room not found!");
+                else return targetRoom;
+            })
+            .then(v => {
+                const stats = {
+                    roomStart: v.room_start,
+                    raceStart: v.race_start,
+                    players: v.members.sort((a, b) => b.ev - a.ev)
+                };
+                stats.highestVR = stats.players[0];
+                stats.lowestVR = stats.players.slice(-1)[0];
+                stats.averageVR = stats.players.reduce((a, b) => a.ev + b.ev) / stats.players.length
+                return stats;
+            });
+    }
 };
+WiimmfiRest.originHost = "https://wiimmfi.de";
+
+module.exports = WiimmfiRest;
