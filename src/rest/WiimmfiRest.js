9aec5a329cd5e74374f21f6512103b9c8c87720d
Timo <artur.fiedler54@gmail.com>
2020-01-09 18:09:14 +0100

    comments and getMKWUsers function

--- src/rest/WiimmfiRest.js
+++ src/rest/WiimmfiRest.js
@@ -5,7 +5,16 @@ module.exports = class WiimmfiRest {
         this.host = host;
     }
 
+    /* SSBB */
     getSSBBStats() {
-        return fetch(`${this.host}/ssbb/amount`).then(v => v.json());
+        return fetch(`${this.host}/ssbb/amount`)
+            .then(v => v.json());
+    }
+
+    /* MKW */
+    getMKWUsers(limit = 10) {
+        return fetch(`${this.hosts}/mkw/users`)
+            .then(v => v.json())
+            .then(v => v.slice(0, limit));
     }
 }
