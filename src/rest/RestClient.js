bee9e142f390872de6fd118d63a5eccdeba1399d
Timo <artur.fiedler54@gmail.com>
2020-01-09 17:47:18 +0100

    add wiimmfirest to restclient

--- src/rest/RestClient.js
+++ src/rest/RestClient.js
@@ -1,10 +1,12 @@
 const RexRest = require("./RexRest");
 const CTGPRest = require ("./CTGPRest");
+const WiimmfiRest = require("./WiimmfiRest");
 const apis = require("../../configs/apis");
 
 module.exports = class RestClient {
     constructor(options = {}) {
         this.rex = new RexRest(options.rex || apis.rexAPI);
         this.ctgp = new CTGPRest(options.ctgp || apis.ctgpAPI);
+        this.wiimmfi = new WiimmfiRest(options.wiimmfi || apis.wiimmfiAPI);
     }
 };
