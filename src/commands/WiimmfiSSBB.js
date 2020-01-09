b51650344cf2b9d7cf8dcdbe702ba8554dcdb317
Timo <artur.fiedler54@gmail.com>
2020-01-09 17:59:33 +0100

    add ssbb command

---
+++ src/commands/WiimmfiSSBB.js
@@ -0,0 +1,36 @@
+module.exports = {
+    name: "ssbb",
+    ownerOnly: false,
+    guildOnly: false,
+    run: async (_, __, rest) => {
+        const res = await rest.wiimmfi.getSSBBStats();
+        return [{
+            embed: {
+                title: "Super Smash Bros. Brawl information",
+                color: res.online === 0 ? 0xFF0000 : 0x00FF00,
+                fields: [
+                    {
+                        name: "Total profiles",
+                        value: res.totalProfiles || 0
+                    },
+                    {
+                        name: "Currently online",
+                        value: res.online || 0
+                    },
+                    {
+                        name: "Logins in the past 30 minutes",
+                        value: res.logins.thirty_minutes || 0
+                    },
+                    {
+                        name: "Logins in the past 4 hours",
+                        value: res.logins.four_hours || 0
+                    },
+                    {
+                        name: "Logins in the past 24 hours",
+                        value: res.logins.twentyfour_hours || 0
+                    }
+                ]
+            }
+        }];
+    }
+};
