f5a12320c2edd0f1c87f55498a032d62eef55a67
Timo <artur.fiedler54@gmail.com>
2020-01-11 01:01:47 +0100

    fix stuff

--- src/commands/CTGPLeaderboard.js
+++ src/commands/CTGPLeaderboard.js
@@ -9,24 +9,34 @@ module.exports = {
             throw new Error("Track name required");
 
         const track = context.trackHelper.tracks.find(v => v.name.toLowerCase() === args.join(" ").toLowerCase());
+        if (!track)
+            throw new Error("Track not found!");
         // TODO: use my improved API that uses cache
         const response = await rest.ctgp.getTrackByEndpoint(track.href, false);
         const uniqueGhosts = [];
+        const pages = [];
 
         for (const ghost of response.ghosts) {
             if (uniqueGhosts.some(v => v.player === ghost.player)) continue;
             else uniqueGhosts.push(ghost);
         }
 
-        return [{
-            embed: {
-                title: "Top 10 ghosts for " + track.name,
-                fields: uniqueGhosts.slice(0, 10).map((v, i) => ({
-                    name: `#${i + 1} ${v.player}`,
-                    value: "Time: `" + v.finishTimeSimple + "`\n"
-                    + "Country: " + (countryCodes[v.country || ""] || "???")
-                }))
-            }
-        }];
+        for (let i = 0; i < uniqueGhosts.length; i += 5) {
+            pages.push({
+                embed: {
+                    title: "Top ghosts for " + track.name,
+                    fields: uniqueGhosts.slice(i, i + 5).map((v, j) => ({
+                        name: `#${i + j + 1} ${v.player}`,
+                        value: "Time: `" + v.finishTimeSimple + "`\n"
+                        + "Country: " + (countryCodes[v.country || ""] || "???")
+                    }))
+                }
+            });
+        }
+
+        context.paginator.createReactionPaginator({
+            pages,
+            message: context.message
+        });
     }
 };
