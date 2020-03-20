ad545f75333b575fe4dcfb447d2dccef71857c80
Timo <btimo8822@gmail.com>
2020-03-20 01:37:55 +0100

    rename file for consistency and add to centralised rest client

--- src/rest/RestClient.js
+++ src/rest/RestClient.js
@@ -1,6 +1,7 @@
 const RexRest = require("./RexRest");
 const CTGPRest = require ("./CTGPRest");
 const WiimmfiRest = require("./WiimmfiRest");
+const TTCRest = require("./TTCRest");
 const apis = require("../../configs/apis");
 
 module.exports = class RestClient {
@@ -8,5 +9,6 @@ module.exports = class RestClient {
         this.rex = new RexRest(options.rex || apis.rexAPI);
         this.ctgp = new CTGPRest(options.ctgp || apis.ctgpAPI);
         this.wiimmfi = new WiimmfiRest(options.wiimmfi || apis.wiimmfiAPI);
+        this.ttc = new TTCRest(options.ttc || apis.ttcAPI);
     }
 };
