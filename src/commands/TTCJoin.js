88e23458f5a8edb1e6a4d8cd6684bf79b80e40c0
Timo <btimo8822@gmail.com>
2020-03-21 21:52:41 +0100

    add ttc join command

---
+++ src/commands/TTCJoin.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "ttc join",
+    guildOnly: false,
+    ownerOnly: false,
+    run: async (context, args, rest) => {
+        const lobby = await rest.ttc.addPlayerToLobby(context.args[1], context.userId, context.channelId);
+        if (lobby.status !== 200) {
+            throw new Error(await lobby.text());
+        }
+
+        return ["Successfully joined lobby!"];
+    }
+};
