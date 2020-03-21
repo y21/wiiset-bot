34a53adfe1f9836d89731c4789a39200a1670f38
Timo <btimo8822@gmail.com>
2020-03-21 21:53:10 +0100

    add ttc leave command

---
+++ src/commands/TTCLeave.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "ttc leave",
+    guildOnly: false,
+    ownerOnly: false,
+    run: async (context, args, rest) => {
+        const lobby = await rest.ttc.removePlayerFromLobby(context.args[1], context.userId, context.channelId);
+        if (lobby.status !== 200) {
+            throw new Error(await lobby.text());
+        }
+
+        return ["Successfully left lobby!"];
+    }
+};
