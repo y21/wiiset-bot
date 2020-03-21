b7b04ae67f93577756a6e0aa4d934ab8f1ee9317
Timo <btimo8822@gmail.com>
2020-03-21 23:59:05 +0100

    use args instead of context.args in ttc submit command

--- src/commands/TTCSubmit.js
+++ src/commands/TTCSubmit.js
@@ -3,7 +3,7 @@ module.exports = {
     guildOnly: false,
     ownerOnly: false,
     run: async (context, args, rest) => {
-        const ghost = await rest.ttc.submitGhost(context.args[1], context.userId, context.channelId);
+        const ghost = await rest.ttc.submitGhost(args[1], context.userId, context.channelId);
         if (ghost.status !== 200) {
             throw new Error(await ghost.text());
         }
