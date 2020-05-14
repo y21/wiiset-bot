128aa99f4b495dec038d5cce1b17a6b2cfb40e2a
Timo <btimo8822@gmail.com>
2020-05-15 00:41:21 +0200

    parse options

--- src/commands/TTCCreateLobby.js
+++ src/commands/TTCCreateLobby.js
@@ -1,9 +1,19 @@
+const { LobbyOptions } = require("../rest/TTCRest");
+
 module.exports = {
     name: "ttc createlobby",
     guildOnly: false,
-    ownerOnly: true,
+    ownerOnly: false,
     run: async (context, args, rest) => {
-        const data = await rest.ttc.createLobby(context.userId, context.channelId);
+        // Parse options
+        const options = args.slice(1)
+            .join(" ")
+            .split(/, */g)
+            .filter(v => LobbyOptions.hasOwnProperty(v))
+            .map(v => LobbyOptions[v])
+            .reduce((a, b) => a | b) || 0;
+
+        const data = await rest.ttc.createLobby(context.userId, context.channelId, options);
         if (data.status !== 200) {
             throw new Error(await data.text());
         }
