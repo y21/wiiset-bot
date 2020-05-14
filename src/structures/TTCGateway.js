27a7406fe7b83e2171d25a88bc4c49c5a74f5deb
Timo <btimo8822@gmail.com>
2020-05-14 22:11:15 +0200

    add title, color and fix rating

--- src/structures/TTCGateway.js
+++ src/structures/TTCGateway.js
@@ -4,6 +4,7 @@ const { ttcWS } = require("../../configs/apis");
 const { ttcToken } = require("../../configs/bot");
 const Logger = require("./Logger");
 const AsciiTable = require("ascii-table");
+const Version = "1.0-beta";
 
 const Events = {
     NewTrack: 0x1,
@@ -58,6 +59,8 @@ module.exports = class TTCGateway {
                     for (const c of message.recipients) {
                         this.bot.client.rest.createMessage(c, {
                             embed: {
+                                title: `TT-Competition ${Version}`,
+                                color: 0x2ecc71,
                                 description: `Preparation phase has started. Boot up Mario Kart Wii and complete a ghost on ${message.data.message}`,
                                 footer: {
                                     text: `Lobby ID: ${message.data.lobbyID}`
@@ -70,7 +73,9 @@ module.exports = class TTCGateway {
                     for (const c of message.recipients) {
                         this.bot.client.rest.createMessage(c, {
                             embed: {
-                                description: "Ingame phase has started! You have 15 minutes to complete a ghost and upload it to the ghost database.",
+                                title: `TT-Competition ${Version}`,
+                                color: 0x2ecc71,
+                                description: "Ingame phase has started! Make sure to upload a ghost to the ghost database on the selected track within the next 15 minutes.",
                                 footer: {
                                     text: `Lobby ID: ${message.data.lobbyID}`
                                 }
@@ -90,10 +95,13 @@ module.exports = class TTCGateway {
                         .filter(v => v.ghost.timeSeconds > 0 && v.ghost.timeSeconds < 360); // > 0 seconds and < 6 minutes
 
                     for (let i = 0; i < filteredGhosts.length; ++i) {
-                        table.addRow(i + 1, filteredGhosts[i].userid, filteredGhosts[i].ghost.finishTimeSimple);
+                        const tag = await this.bot.rest.fetchUser(filteredGhosts[i].userid).then(v => v.tag);
+                        table.addRow(i + 1, tag, filteredGhosts[i].ghost.finishTimeSimple);
                     }
 
                     const embed = {
+                        title: `TT-Competition ${Version}`,
+                        color: 0x2ecc71,
                         fields: [
                             {
                                 name: "Top ghosts",
@@ -101,7 +109,7 @@ module.exports = class TTCGateway {
                             },
                             {
                                 name: "Eliminated",
-                                value: message.data.eliminated.map((v, i) => `<@${v.userid}> (${v.total_rating}R)`).join("\n") || "No players have been eliminated"
+                                value: message.data.eliminated.map((v, i) => `<@${v.userid}> (${v.total_rating + v.base_rating}R)`).join("\n") || "No players have been eliminated"
                             }
                         ]
                     };
@@ -114,7 +122,9 @@ module.exports = class TTCGateway {
                     for (const c of message.recipients) {
                         this.bot.client.rest.createMessage(c, {
                             embed: {
-                                description: `Lobby has ended! Winner: <@${message.data.winner.userid}> (${message.data.winner.total_rating}R)`,
+                                title: `TT-Competition ${Version}`,
+                                color: 0x2ecc71,
+                                description: `Lobby has ended! Winner: <@${message.data.winner.userid}> (${message.data.winner.total_rating + message.data.winner.base_rating}R)`,
                                 color: 0x2ecc71,
                                 footer: {
                                     text: `Lobby ID: ${message.data.lobbyID}`
