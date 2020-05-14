c1ff94fcdc170251b1dde07bea2f3815a75c5f41
Timo <btimo8822@gmail.com>
2020-05-14 22:12:33 +0200

    display time

--- src/commands/TTCSubmit.js
+++ src/commands/TTCSubmit.js
@@ -1,15 +1,16 @@
 module.exports = {
     name: "ttc submit",
     guildOnly: false,
-    ownerOnly: true,
+    ownerOnly: false,
     run: async (context, args, rest) => {
         const ghost = await rest.ttc.submitGhost(args[1], context.userId, context.channelId);
         if (ghost.status !== 200) {
             throw new Error(await ghost.text());
         }
+        const data = await ghost.json();
 
 
         // TODO: Display time of ghost
-        return ["Successfully submitted ghost!"];
+        return [`Successfully submitted ghost! Time: ${data.finishTimeSimple}`];
     }
 };
