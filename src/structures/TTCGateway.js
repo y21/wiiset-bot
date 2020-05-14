b8c0e92bd57d7d91a471906e0209139cf1e22dd0
Timo <btimo8822@gmail.com>
2020-05-14 02:53:19 +0200

    implement more gateway events

--- src/structures/TTCGateway.js
+++ src/structures/TTCGateway.js
@@ -1,52 +1,124 @@
-const pws = require("pws");
-const ws = require("ws");
-const { ttcAPI } = require("../../configs/apis");
-const Logger = require("./structures/Logger");
+const PersistentWebSocket = require("pws");
+const WebSocket = require("ws");
+const { ttcWS } = require("../../configs/apis");
+const { ttcToken } = require("../../configs/bot");
+const Logger = require("./Logger");
+const AsciiTable = require("ascii-table");
 
 const Events = {
     NewTrack: 0x1,
-    GameStart: 0x2
+    GameStart: 0x2,
+    RoundEnd: 0x4,
+    LobbyEnd: 0x8
+};
+
+const LobbyStates = {
+    Waiting: 0x1,
+    MapPick: 0x2,
+    Preparation: 0x4,
+    Ingame: 0x8,
+    Upload: 0x10
 };
 
 module.exports = class TTCGateway {
     constructor(bot) {
         this.bot = bot;
-        this.connection = new pws(ttcAPI, ws);
+        this.connection = new PersistentWebSocket(ttcWS, WebSocket);
         this.ready = false;
     }
 
     init() {
-        this.connection.onready = () => {
+        this.connection.onopen = () => {
+            console.log("Sent IDENTIFY call");
             this.ready = true;
+            this.connection.send(JSON.stringify({
+                t: "IDENTIFY",
+                d: {
+                    key: ttcToken
+                }
+            }));
         };
 
         this.connection.onclose = () => {
+            console.log("closed");
             this.ready = false;
         };
 
         this.connection.onmessage = ({ data: event }) => {
-            let data;
+            // TODO: clean this mess up
+            let message;
             try {
-                data = JSON.parse(event);
+                message = JSON.parse(event);
             } catch(e) {
                 return Logger.error(e.message);
             }
 
-            switch(data.type) {
+            switch(message.type) {
                 case Events.NewTrack:
-                    for (const c of data.recipients) {
+                    for (const c of message.recipients) {
                         this.bot.client.rest.createMessage(c, {
                             embed: {
-                                description: `Preparation phase has started. Boot up Mario Kart Wii and complete a ghost on ${data.data.message}`
+                                description: `Preparation phase has started. Boot up Mario Kart Wii and complete a ghost on ${message.data.message}`,
+                                footer: {
+                                    text: `Lobby ID: ${message.data.lobbyID}`
+                                }
                             }
                         });
                     }
                 break;
-                case Events.NewTrack:
-                    for (const c of data.recipients) {
+                case Events.GameStart:
+                    for (const c of message.recipients) {
                         this.bot.client.rest.createMessage(c, {
                             embed: {
-                                description: "Ingame phase has started! You have 15 minutes to complete a ghost and upload it to the ghost database.\n"
+                                description: "Ingame phase has started! You have 15 minutes to complete a ghost and upload it to the ghost database.",
+                                footer: {
+                                    text: `Lobby ID: ${message.data.lobbyID}`
+                                }
+                            }
+                        });
+                    }
+                break;
+                case Events.RoundEnd:
+                    const table = new AsciiTable()
+                        .removeBorder()
+                        .setHeading("#", "Player", "Time");
+
+
+                    const filteredGhosts = message.data.remainingPlayers
+                        .concat(message.data.eliminated)
+                        .sort((a, b) => a.ghost.timeSeconds - b.ghost.timeSeconds)
+                        .filter(v => v.ghost.timeSeconds > 0 && v.ghost.timeSeconds < 360); // > 0 seconds and < 6 minutes
+
+                    for (let i = 0; i < filteredGhosts.length; ++i) {
+                        table.addRow(i + 1, filteredGhosts[i].userid, filteredGhosts[i].ghost.finishTimeSimple);
+                    }
+
+                    const embed = {
+                        fields: [
+                            {
+                                name: "Top ghosts",
+                                value: "```js\n" + (table.toString().trim() || "No ghosts found") + "\n```"
+                            },
+                            {
+                                name: "Eliminated",
+                                value: message.data.eliminated.map((v, i) => `<@${v.userid}> (${v.total_rating}R)`).join("\n") || "No players have been eliminated"
+                            }
+                        ]
+                    };
+
+                    for (const c of message.recipients) {
+                        this.bot.client.rest.createMessage(c, { embed });
+                    }
+                break;
+                case Events.LobbyEnd:
+                    for (const c of message.recipients) {
+                        this.bot.client.rest.createMessage(c, {
+                            embed: {
+                                description: `Lobby has ended! Winner: <@${message.data.winner.userid}> (${message.data.winner.total_rating}R)`,
+                                color: 0x2ecc71,
+                                footer: {
+                                    text: `Lobby ID: ${message.data.lobbyID}`
+                                }
                             }
                         });
                     }
@@ -54,4 +126,9 @@ module.exports = class TTCGateway {
             }
         };
     }
+}
+
+function calculateGainForPlacement(placement, totalPlayers) {
+    // gain := int((totalPlayers * 35) / placement)
+    return (totalPlayers * 35) / placement;
 }
