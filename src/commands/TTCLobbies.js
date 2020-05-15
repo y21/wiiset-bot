697e66d1aa399bbaea34c8e7e13dd82f4587f41d
Timo <btimo8822@gmail.com>
2020-05-15 17:00:03 +0200

    sort players

--- src/commands/TTCLobbies.js
+++ src/commands/TTCLobbies.js
@@ -1,3 +1,5 @@
+const { stateToString } = require("../rest/TTCRest");
+
 module.exports = {
     name: "ttc lobbies",
     guildOnly: false,
@@ -8,16 +10,15 @@ module.exports = {
             throw new Error(await lobbies.text());
         }
 
-        const data = await lobbies.json();
+        const data = await lobbies.json().then(v => v.sort((a, b) => b.players - a.players).slice(0, 10));
 
         // TODO: Use paginator to allow more than 10 lobbies
-
         return [{
             embed: {
                 color: 0x2ecc71,
                 fields: data.slice(0, 10).map(v => ({
                     name: v.id,
-                    value: "Players: " + v.players.length
+                    value: `State: ${stateToString(v.state)}\nPlayers: ${v.players.length}\nRound: ${v.round}`
                 })),
                 footer: {
                     text: "Join a lobby using w.ttc join <id>"
