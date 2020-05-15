608c058a68c7c3de9cb81b2315ab747b22769e5e
Timo <btimo8822@gmail.com>
2020-05-15 16:59:01 +0200

    add helper functions for states

--- src/rest/TTCRest.js
+++ src/rest/TTCRest.js
@@ -1,4 +1,82 @@
 const fetch = require("node-fetch");
+const { LobbyStates } = require("../structures/TTCGateway");
+
+const LobbyOptions = {
+    RT: 0x1,
+    CT: 0x2,
+    AT: 0x1 | 0x2,
+    "150cc": 0x8,
+    "200cc": 0x10,
+    NoElimination: 0x20
+};
+
+/**
+ * Formats lobby options
+ * 
+ * @param {number} options
+ * @returns {string}
+ */
+function formatLobbyOptions(options) {
+    const allOptions = [];
+    
+    if (hasOption(options, LobbyOptions["150cc"]))
+        allOptions.push("150cc");
+
+    if (hasOption(options, LobbyOptions["200cc"]))
+        allOptions.push("200cc");
+
+    if (hasOption(options, LobbyOptions.AT))
+        allOptions.push("All Tracks");
+    else if (hasOption(options, LobbyOptions.RT))
+        allOptions.push("RTs");
+    else if (hasOption(options, LobbyOptions.CT))
+        allOptions.push("CTs");
+
+    if (hasOption(options, LobbyOptions.NoElimination))
+        allOptions.push("No Elimination");
+    
+
+    return allOptions.join(", ");
+}
+
+/**
+ * Checks whether an option is selected
+ * 
+ * @param {number} options 
+ * @param {number} option
+ * @returns {boolean}
+ */
+function hasOption(options, option) {
+    return (options & option) === option;
+}
+
+/**
+ * Formats a lobby state
+ * @param {number} state
+ * @returns {string?}
+ */
+function stateToString(state) {
+    switch (state) {
+        case LobbyStates.Waiting:
+            return "Waiting";
+            break;
+        case LobbyStates.ThresholdReached:
+            return "Waiting for more players"
+            break;
+        case LobbyStates.MapPick:
+            return "Picking map"
+            break;
+        case LobbyStates.Preparation:
+            return "Preparing";
+            break;
+        case LobbyStates.Ingame:
+            return "Ingame";
+            break;
+        case LobbyStates.Upload:
+            return "Uploading ghosts";
+            break;
+    }
+}
 
 module.exports = class TTCRest {
     constructor(host) {
@@ -78,11 +156,6 @@ module.exports = class TTCRest {
     }
 }
 
-module.exports.LobbyOptions = {
-    RT: 0x1,
-    CT: 0x2,
-    AT: 0x1 | 0x2,
-    "150cc": 0x8,
-    "200cc": 0x10,
-    NoElimination: 0x20
-};
+module.exports.LobbyOptions = LobbyOptions;
+module.exports.formatLobbyOptions = formatLobbyOptions;
+module.exports.stateToString = stateToString;
