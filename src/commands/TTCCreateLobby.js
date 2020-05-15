31bd0ff56299d5a8364f1c1873c563774df5bd82
Timo <btimo8822@gmail.com>
2020-05-15 17:00:32 +0200

    add user input checking

--- src/commands/TTCCreateLobby.js
+++ src/commands/TTCCreateLobby.js
@@ -1,17 +1,20 @@
-const { LobbyOptions } = require("../rest/TTCRest");
+const { LobbyOptions, formatLobbyOptions } = require("../rest/TTCRest");
 
 module.exports = {
     name: "ttc createlobby",
     guildOnly: false,
-    ownerOnly: false,
+    ownerOnly: true,
     run: async (context, args, rest) => {
+        if (args.length < 2)
+            throw new Error("no lobby options given, available options: " + Object.keys(LobbyOptions).join(", ") + " (use \",\" as delimiter for more than one option)");
+
         // Parse options
         const options = args.slice(1)
             .join(" ")
             .split(/, */g)
             .filter(v => LobbyOptions.hasOwnProperty(v))
             .map(v => LobbyOptions[v])
-            .reduce((a, b) => a | b) || 0;
+            .reduce((a, b) => a | b);
 
         const data = await rest.ttc.createLobby(context.userId, context.channelId, options);
         if (data.status !== 200) {
@@ -29,6 +32,10 @@ module.exports = {
                         {
                             name: "Lobby ID",
                             value: lobby.id
+                        },
+                        {
+                            name: "Lobby Options",
+                            value: formatLobbyOptions(lobby.options) || "-"
                         }
                     ]
                 }
