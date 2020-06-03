83e5ef0ab6cd31eae66edfa73fb181067b7fb017
Timo <btimo8822@gmail.com>
2020-06-03 16:55:55 +0200

    fix users command

--- src/commands/MKWUsers.js
+++ src/commands/MKWUsers.js
@@ -1,21 +1,38 @@
+const { Markup } = require("detritus-client/lib/utils");
+const AsciiTable = require("ascii-table");
+
 module.exports = {
     name: "mkwusers",
     ownerOnly: false,
     guildOnly: false,
-    run: async (context, __, rest) => {
-        const req = await rest.wiimmfi.getMKWUsers(-1);
+    run: async (context, args, rest) => {
+        const req = await rest.wiimmfi.getMKWUsers(args[0] || undefined, -1);
         const pages = [];
 
         if (req.length === 0) {
             throw new Error("no rooms found");
         }
 
-        for (let i = 0; i < req.length; i += 10)
+        for (let i = 0; i < req.length; i += 10) {
+            const table = new AsciiTable()
+                .removeBorder()
+                .setHeading("Player", "", "VR");
+
+            for (const player of req.slice(i, i + 10)) {
+                const miiName = (player.names || ["<unknown>"])[0];
+                const vr = player.ev === -1 ? "-" : player.ev;
+                table.addRow(miiName, "", vr);
+            }
+
             pages.push({
                 embed: {
-                    description: "```\n" + req.slice(i, i + 10).join("\n") + "\n```"
+                    description: Markup.codeblock(table.toString(), {
+                        language: "js",
+                        limit: 1990
+                    })
                 }
             });
+        }
 
         context.paginator.createReactionPaginator({
             message: context.message,
