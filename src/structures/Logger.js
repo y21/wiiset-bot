e018a88f1451b2252715e3d4e3d8e239fe3c4aac
Timo <btimo8822@gmail.com>
2020-06-03 19:22:12 +0200

    don't waste LoC

--- src/structures/Logger.js
+++ src/structures/Logger.js
@@ -1,8 +1,4 @@
 module.exports = {
-    error: (text) => {
-        console.log("\x1b[31m%s\x1b[0m", text);
-    },
-    info: (text) => {
-        console.log("\x1b[34m%s\x1b[0m", text);
-    }
+    error: (text) => console.log("\x1b[31m%s\x1b[0m", text),
+    info: (text) => console.log("\x1b[34m%s\x1b[0m", text)
 };
