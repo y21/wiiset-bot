1081a8eb102433b56839fdb9d143bddbb59149b1
Timo <btimo8822@gmail.com>
2020-06-14 00:27:24 +0200

    gateway cleanup

--- src/structures/ttc/Gateway.js
+++ src/structures/ttc/Gateway.js
@@ -31,12 +31,11 @@ const LobbyStates = {
 const Medals = [ "🥇", "🥈", "🥉" ];
 
 const Texts = {
-    PreparationPhase: "ℹ️ Preparation phase has started. Boot up Mario Kart Wii and complete a ghost on {track}",
+    PreparationPhase: "ℹ️ Preparation phase has started. Boot up Mario Kart Wii and complete a ghost on {track}.\nWorld Record: {time}",
     ThresholdPhase: "⏱️ Minimum number of players for this lobby has been reached, waiting 30 more seconds for more players to join...",
     IngamePhase: "🏎️ Ingame phase has started! Make sure to upload a ghost to the ghost database on the selected track within the next 15 minutes.",
     LobbyTimeWarning: "⚠️ {time} left!",
-    LobbyEndUser: "✅ Lobby has ended! Winner: __{winner}__ ({rating}R)",
-    LobbyEndTeams: "✅ Lobby has ended! Winner: __Team #{winner}__\n\n"
+    LobbyEnd: "✅ Lobby has ended! Results:\n\n"
 };
 Texts.LobbyTimeWarningLast = Texts.LobbyTimeWarning + " Please make sure to upload a ghost to the ghost database.";
 
@@ -86,7 +85,9 @@ module.exports = class TTCGateway {
 
             switch(message.type) {
                 case Events.NewTrack:
-                    messageData.embed.description = Texts.PreparationPhase.replace("{track}", message.data.message);
+                    messageData.embed.description = Texts.PreparationPhase
+                        .replace("{track}", message.data.message)
+                        .replace("{time}", timeSecondsToString(message.data.wrTime));
                 break;
                 case Events.ThresholdReached:
                     messageData.embed.description = Texts.ThresholdPhase;
@@ -94,31 +95,11 @@ module.exports = class TTCGateway {
                 case Events.GameStart:
                     messageData.embed.description = Texts.IngamePhase;
                 break;
-                case Events.RoundEnd:
-                    const table = new AsciiTable()
-                        .removeBorder()
-                        .setHeading("#", "Player", "Time");
-
-                    const filteredGhosts = message.data.remainingPlayers
-                        .concat(message.data.eliminated)
-                        .sort((a, b) => a.ghost.timeSeconds - b.ghost.timeSeconds)
-                        .filter(v => v.ghost.timeSeconds > 0 && v.ghost.timeSeconds < 360); // > 0 seconds and < 6 minutes
-
-                    for (let i = 0; i < filteredGhosts.length; ++i) {
-                        const player = filteredGhosts[i];
-                        let tag, finishTime;
-
-                        if (player.aiDiff === User.AiDifficulty.DISABLED) {
-                            tag = await this.bot.client.rest.fetchUser(player.userid).then(v => v.username);
-                            finishTime = player.ghost.finishTimeSimple;
-                        } else {
-                            const { aiDiff } = player;
-                            tag = User.buildAIName(player.userid, aiDiff);
-                            finishTime = timeSecondsToString(player.ghost.timeSeconds);
-                        }
-
-                        table.addRow(Medals[i] || i + 1, tag, finishTime);
-                    }
+                case Events.RoundEnd: {
+                    const table = await this.generateResultsTable(message.data.remainingPlayers.concat(message.data.eliminated), message.data.wrTime, {
+                        end: false,
+                        isTeamsMode: false
+                    });
 
                     messageData.embed.fields = [
                         {
@@ -132,22 +113,25 @@ module.exports = class TTCGateway {
                                 .join("\n") || "No players have been eliminated"
                         }
                     ];
-                break;
-                case Events.LobbyEnd:
+                    break;
+                }
+                case Events.LobbyEnd: {
                     const { winner } = message.data;
+                    const players = !message.data.isTeamsMode ? message.data.users : Object.values(message.data.users);
+                    const table = await this.generateResultsTable(players.slice(0, 10), null, {
+                        end: true,
+                        isTeamsMode: message.data.isTeamsMode
+                    });
 
-                    if (message.data.isTeamsMode) {
-                        messageData.embed.description = Texts.LobbyEndTeams
-                            .replace("{winner}", winner.id) + 
-                            Lobby.teamsToString(Object.values(message.data.users), true)
-                    } else {
-                        messageData.embed.description = Texts.LobbyEndUser
-                            .replace("{winner}", winner.aiDiff !== User.AiDifficulty.DISABLED ? User.buildAIName(winner.userid, winner.aiDiff) : "<@" + winner.userid + ">")
-                            .replace("{rating}", message.data.winner.total_rating + message.data.winner.base_rating);
-                    }
-                break;
-                case Events.LobbyWarning:
-                    const { remaining, warnCount } = message.data;
+
+                    messageData.embed.description = Texts.LobbyEnd +
+                        "```js\n" +
+                        table.toString() +
+                        "\n```"
+                    break;
+                }
+                case Events.LobbyWarning: {
+                    const {remaining, warnCount} = message.data;
                     let timeF;
 
                     if (remaining < 60) {
@@ -159,6 +143,7 @@ module.exports = class TTCGateway {
                     messageData.embed.description = Texts[warnCount >= 2 ? "LobbyTimeWarningLast" : "LobbyTimeWarning"]
                         .replace("{time}", timeF);
                     break;
+                }
             }
 
             // Ugly check to see if embed has changed
@@ -169,13 +154,92 @@ module.exports = class TTCGateway {
             }
         };
     }
+
+    /**
+     * Generates a table for round results
+     *
+     * @param {any[]} players
+     * @param {boolean} end
+     * @returns {AsciiTable}
+     */
+    async generateResultsTable(players, wrTime, { end, isTeamsMode }) {
+        const table = new AsciiTable()
+            .removeBorder();
+
+        const playersHeading = isTeamsMode && end ? "Team" : "Player";
+        let fastestGhosts = players;
+
+        if (end && !isTeamsMode) {
+            fastestGhosts = fastestGhosts.sort((a, b) => b.points - a.points);
+            table.setHeading("#", playersHeading, "Pts");
+        } else if (end && isTeamsMode) {
+            for (const team of fastestGhosts) {
+                team.points = team.players.reduce((a, b) => a + b.points, 0)
+            }
+            fastestGhosts = fastestGhosts.sort((a, b) => b.points - a.points);
+            table.setHeading("#", playersHeading, "Pts");
+        } else {
+            fastestGhosts = fastestGhosts.sort((a, b) => b.points - a.points);
+            table.setHeading("#", playersHeading, "Time", "Pts");
+        }
+
+        for (let i = 0; i < fastestGhosts.length; ++i) {
+            const player = fastestGhosts[i];
+            let tag, finishTime, points = player.points | 0;
+
+            if (!isTeamsMode) {
+                if (player.aiDiff === User.AiDifficulty.DISABLED) {
+                    tag = await this.bot.client.rest.fetchUser(player.userid).then(v => v.username);
+                    finishTime = player.ghost.finishTimeSimple;
+                } else {
+                    const {aiDiff} = player;
+                    tag = User.buildAIName(player.userid, aiDiff);
+                    finishTime = timeSecondsToString(player.ghost.timeSeconds);
+                }
+            } else {
+                tag = `Team ${player.id}`;
+                points = player.points;
+            }
+
+            if (!end) {
+                if (player.ghost.timeSeconds > 360) { // no ghost?
+                    finishTime = "-";
+                    points += " (+0)";
+                } else {
+                    points += ` (+${calculatePoints(player.ghost.timeSeconds - wrTime) | 0})`;
+                }
+
+                table.addRow(Medals[i] || i + 1, tag, finishTime, points);
+            } else {
+                table.addRow(Medals[i] || i + 1, tag, points);
+            }
+        }
+
+        return table;
+    }
 }
 
+/**
+ * Formats `timeSeconds`
+ *
+ * @param {number} time
+ * @returns {string}
+ */
 function timeSecondsToString(time) {
     const min = time / 60 | 0;
     const sec = time % 60;
     const ms = sec - (sec | 0);
     return String(min).padStart(2, "0") + ":" + String(sec | 0).padStart(2, 0) + (String(ms).slice(1, 5) || ".000");
+}
+
+/**
+ * Calculates points for a player
+ *
+ * @param {number} timeOffWr
+ * @returns {number}
+ */
+function calculatePoints(timeOffWr) {
+    return Math.max(15 - (1.5 * timeOffWr), 3);
 }
 
 module.exports.LobbyStates = LobbyStates;
