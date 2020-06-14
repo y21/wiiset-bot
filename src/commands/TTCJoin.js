d39e5999d57bb9c08aa4e7d0f2f6c159a25400c8
Timo <btimo8822@gmail.com>
2020-06-14 18:57:41 +0200

    validate w.ttc join argument

--- src/commands/TTCJoin.js
+++ src/commands/TTCJoin.js
@@ -6,6 +6,9 @@ module.exports = {
         description: "Joins a TTC Lobby"
     },
     run: async (context, args, rest) => {
+        if (args[0] && isNaN(args[0])) {
+            throw new Error("Invalid Lobby ID");
+        }
         await rest.ttc.addPlayerToLobby(args[0], context.userId, context.channelId, args[1]);
 
         return ["Successfully joined lobby!"];
