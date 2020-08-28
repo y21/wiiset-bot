28151f47cad4918386a78af8d9f6c552eb749d88
Timo <btimo8822@gmail.com>
2020-08-28 18:27:59 +0000

    add error message to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 export const PAGINATOR_TIME_LIMIT = 300_000;
 
 export const SNOWFLAKE_REGEX = new RegExp('^\\d{17,19}$');
+export const PID_REGEX = new RegExp('^[A-F\\d]{16}$', 'i');
 
 export const TTC_VERSION = '1.1-beta';
 export const TTC_AUTO_DETECT = 0;
@@ -9,3 +10,17 @@ export const NUMBER_EMOJIS = [ "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️
 export const MEDALS = [ "🥇", "🥈", "🥉" ];
 
 export const UNKNOWN_CMD_ERROR = ':x: Command execution has been cancelled';
+export const GUILD_OWNER_CMD = 'Only the owner of this server can run this command.';
+
+export const DELIMITER = '-';
+
+export interface Page {
+    embed: {
+        title: string,
+        fields: Array<{
+            name: string,
+            value: string
+        }>,
+        inline?: boolean
+    }
+}
